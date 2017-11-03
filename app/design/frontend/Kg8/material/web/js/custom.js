2105100169ac73a99c237a81faea4a094160ab7d
navrocky-sergey <sergey@ecomitize.com>
2017-11-03 14:16:53 +0200

    start project

---
+++ app/design/frontend/Kg8/material/web/js/custom.js
@@ -0,0 +1,6 @@
+require([
+    'jquery',
+    'materialize'
+], function ($, script) {
+    $('body').append($materialize)
+});
---
+++ app/design/frontend/Kg8/material/web/js/materialize.js
@@ -0,0 +1,9 @@
+define([
+    "jquery"
+],
+function($) {
+    "use strict";
+
+    // Here your custom code...
+    console.log('Ura! materialize.js');
+});
